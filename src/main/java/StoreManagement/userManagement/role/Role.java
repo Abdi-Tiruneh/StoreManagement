1d00e4520977a88dc26b8db4118f3bb0e81ccc01
ABDI TIRUNEH <abdi.tirunehh@gmail.com>
2023-10-20 15:36:37 +0300

    add user role

---
+++ src/main/java/StoreManagement/userManagement/role/Role.java
@@ -0,0 +1,37 @@
+package StoreManagement.userManagement.role;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "roles")
+@Data
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Long roleId;
+
+    @Column(nullable = false, unique = true)
+    private String roleName;
+
+    private String description;
+
+    @CreationTimestamp
+    @Column(name = "registered_at")
+    private LocalDateTime registeredAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    public Role(String roleName,String description) {
+        this.roleName = roleName;
+        this.description = description;
+    }
+}
+
