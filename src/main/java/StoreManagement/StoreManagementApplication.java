892e6e574785207b4528460df6c076a0ad4e975a
ABDI TIRUNEH <abdi.tirunehh@gmail.com>
2023-10-25 16:50:50 +0300

    enable async operations

--- src/main/java/StoreManagement/StoreManagementApplication.java
+++ src/main/java/StoreManagement/StoreManagementApplication.java
@@ -2,8 +2,10 @@ package StoreManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync(proxyTargetClass = true)
 public class StoreManagementApplication {
 
 	public static void main(String[] args) {
